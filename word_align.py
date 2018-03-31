0850f737d29d5fd177ab484f875814db3f087e74
yimingw2 <yimingw2@cs.cmu.edu>
2018-03-31 18:06:05 -0400

    the newest version

--- word_align.py
+++ word_align.py
@@ -13,13 +13,12 @@ from fuzzy_match_word import fuzzy_match
 class Alignment():
 
 
-	def __init__(self, label, recog_file_path, trans_file_path, output_stm_path, output_ctm_path, noise_itv):
+	def __init__(self, label, recog_file_path, trans_file_path, output_stm_path, noise_itv):
 		self.label = label
 		self.recog_file_path = recog_file_path
 		self.trans_file_path = trans_file_path
 		self.output_stm_path = output_stm_path
-		self.output_ctm_path = output_ctm_path
-		self.noise_itv = noise_itv # [[t1, t2], [t3]]
+		self.noise_itv = noise_itv  # [[t1, t2], [t3]]
 
 
 	def _none_word(self, s):
@@ -39,34 +38,31 @@ class Alignment():
 			if len(ts) == 2:
 				time1 = ts[0]
 				time2 = ts[1]
-				if t >= time1 and t <= time2:
+				if time1 <= t and t <= time2:
 					return True
 			else:
 				end_time = ts[0]
 				if t >= end_time:
 					return True
-
 		return False
 
 
 	def _process_recog(self):
 		"""
-		Process the recognition CTM file
+		Process the recognition .ctm file
 		:param: 
-		:return: 
+		:return: a recog_list with format [word, global_time, time_inv]
 		"""
 		recog_list = list()
-		int_f = open(self.recog_file_path, 'r')
-		f = iter(int_f)
-		for line in f:
-			part = line.split()
-			if not (float(part[3]) == 0 or self._none_word(part[4]) == True):
-				time_global = float(part[2])
-				if self._is_noise(time_global):
-					continue
-				recog_list.append([part[4], time_global, float(part[3])]) # [word, time_global, time_inv]
-		int_f.close()
-
+		with open(self.recog_file_path, 'r') as int_f:
+			f = iter(int_f)
+			for line in f:
+				part = line.split()
+				if not (float(part[3]) == 0 or self._none_word(part[4]) == True):
+					time_global = float(part[2])
+					if self._is_noise(time_global):
+						continue
+					recog_list.append([part[4], time_global, float(part[3])]) # [word, time_global, time_inv]
 		return recog_list
 
 
@@ -74,28 +70,26 @@ class Alignment():
 		"""
 		Process the transcription TXT file
 		:param: 
-		:return: 
+		:return: a word_list with format [word, 0, 0]
 		"""
-		in_f = open(self.trans_file_path, 'r')
-		f = iter(in_f)
 		t_word = list()
-		for line in f:
-			word = line.lower().split();
-			if len(word) == 0 or len(word) == 1:
-				continue
-			'''
-			if w_0[len(w_0)-1] == '+': # ignore the label starting with #pat+ || #doc+
-				continue;
-			'''
-			for i in range(len(word)):
-				if i != 0: # exclude the channel
-					w = word[i]
-					if w == "<name>":
-						continue
-					else:
-						t_word.append([w, 0, 0])
-		in_f.close()
-
+		with open(self.trans_file_path, 'r') as in_f:
+			f = iter(in_f)
+			for line in f:
+				word = line.lower().split();
+				if len(word) == 0 or len(word) == 1:
+					continue
+				'''
+				if w_0[len(w_0)-1] == '+': # ignore the label starting with #pat+ || #doc+
+					continue;
+				'''
+				for i in range(len(word)):
+					if i != 0: # exclude the channel
+						w = word[i]
+						if w == "<name>":
+							continue
+						else:
+							t_word.append([w, 0, 0])
 		return t_word
 
 
@@ -140,20 +134,20 @@ class Alignment():
 					if len(ts) == 2:						
 						time1 = ts[0]
 						time2 = ts[1]
-						if s_time < time1 and e_time > time2:
+						if s_time < time1 and time2 < e_time:
 							e_time = min(e_time, time1)
 					else:
 						time0 = ts[0]
-						if time0 > time1 and time0 < time2:
+						if s_time < time0 and time0 < e_time:
 							e_time = min(e_time, time0)
 
 				char_len = 0
 				for i in range(i_s, i_e):
 					char_len += len(self.trans_t_dict[i][0])
-				#ratio = float(self.trans_t_dict[i_e][1]-self.trans_t_dict[i_s][1]) / float(char_len)
+				# ratio = float(self.trans_t_dict[i_e][1]-self.trans_t_dict[i_s][1]) / float(char_len)
 				ratio = float(e_time - s_time) / float(char_len)
 				char_len = 0
-				s_time = self.trans_t_dict[i_s][1]
+				# s_time = self.trans_t_dict[i_s][1]
 				for i in range(i_s+1, i_e):
 					char_len += len(self.trans_t_dict[i-1][0])
 					self.trans_t_dict[i][1] = s_time + char_len * ratio
@@ -162,56 +156,64 @@ class Alignment():
 
 
 	def output_align_sentence(self):
-		input_f = open(self.trans_file_path, 'r')
-		output_f_stm = open(self.output_stm_path, 'w')
-		output_f_ctm = open(self.output_ctm_path, 'w')
-		f = iter(input_f)
-		idx = 0
-		for line in f:
-			word = line.lower().split()
-			label_tag = word[0]
-			if label_tag == "#doc#" or label_tag == "#doc+":
-				label_tag = 1
-			elif label_tag == "#pat#" or label_tag == "#pat+":
-				label_tag = 2
-			else:
-				label_tag = 3
 
-			word = word[1:]
-			line_output = list()
-			start_time = 0
-			end_time = 0
-			word_idx = list()
-			for i in range(len(word)):
-				if word[i] != "<name>":
-					word_idx.append(idx)
-					line_output.append(word[i])
-					line_output.append(" ")
-					idx += 1
+		with open(self.trans_file_path, 'r') as input_f, \
+			 open(self.output_stm_path, 'w') as output_f_stm:
 
-			# for stm file
-			if len(word_idx) != 0:
-				start_time = self.trans_t_dict[word_idx[0]][1]
-				tmp_e = self.trans_t_dict[word_idx[len(word_idx)-1]][1]+self.trans_t_dict[word_idx[len(word_idx)-1]][2]
-				if word_idx[len(word_idx)-1] == len(self.trans_t_dict)-1 or tmp_e < self.trans_t_dict[word_idx[len(word_idx)-1]+1][1]:
-					end_time = tmp_e
+			f = iter(input_f)
+			idx = 0
+			for line in f:
+				word = line.lower().split()
+				label_tag = word[0]
+				if label_tag == "#doc#" or label_tag == "#doc+":
+					label_tag = 1
+				elif label_tag == "#pat#" or label_tag == "#pat+":
+					label_tag = 2
 				else:
-					end_time = self.trans_t_dict[word_idx[len(word_idx)-1]+1][1]
-				# since for ASR it should have 0.01s to recognize
-				if end_time - start_time < 0.01:
-					end_time = start_time + 0.01
-				output_f_stm.write('%s %d %s %.3f %.3f <none> %s\n' % (self.label, label_tag, self.label, start_time, end_time, "".join(line_output)))
+					label_tag = 3
 
-			# for ctm file
-			for i in word_idx:
-				output_f_ctm.write("%s %d %.3f %.3f %s 1\n" % (self.label, label_tag, self.trans_t_dict[i][1], self.trans_t_dict[i][2], self.trans_t_dict[i][0]))
-			if len(word_idx) != 0:
-				output_f_ctm.write("%s %d %.3f 0 <#s> 1\n" % (self.label, label_tag, self.trans_t_dict[word_idx[len(word_idx)-1]][1]+self.trans_t_dict[word_idx[len(word_idx)-1]][2]))
+				word = word[1:]
+				line_output = list()
+				start_time = 0
+				end_time = 0
+				word_idx = list()
+				for i in range(len(word)):
+					if word[i] != "<name>":
+						word_idx.append(idx)
+						line_output.append(word[i])
+						line_output.append(" ")
+						idx += 1
 
-		input_f.close()
-		output_f_stm.close()
-		output_f_ctm.close()
+				# for stm file
+				if len(word_idx) != 0:
+					start_time = self.trans_t_dict[word_idx[0]][1]
+					tmp_e = self.trans_t_dict[word_idx[len(word_idx)-1]][1]+self.trans_t_dict[word_idx[len(word_idx)-1]][2]
+					if word_idx[len(word_idx)-1] == len(self.trans_t_dict)-1 or tmp_e < self.trans_t_dict[word_idx[len(word_idx)-1]+1][1]:
+						end_time = tmp_e
+					else:
+						end_time = self.trans_t_dict[word_idx[len(word_idx)-1]+1][1]
+					# since for ASR it should have 0.01s to recognize
+					if end_time - start_time < 0.01:
+						end_time = start_time + 0.01
+					# ignore all the sentence which include a noise interval
+					contain_noise = False
+					for ts in self.noise_itv:
+						if len(ts) == 2:
+							time1 = ts[0]
+							time2 = ts[1]
+							if start_time < time1 and time2 < end_time:
+								contain_noise = True
+								break
+					if not contain_noise:
+						output_f_stm.write('%s %d %s %.3f %.3f <none> %s\n' % (self.label, label_tag, self.label, start_time, end_time, "".join(line_output)))
 
+				"""
+				# for ctm file
+				for i in word_idx:
+					output_f_ctm.write("%s %d %.3f %.3f %s 1\n" % (self.label, label_tag, self.trans_t_dict[i][1], self.trans_t_dict[i][2], self.trans_t_dict[i][0]))
+				if len(word_idx) != 0:
+					output_f_ctm.write("%s %d %.3f 0 <#s> 1\n" % (self.label, label_tag, self.trans_t_dict[word_idx[len(word_idx)-1]][1]+self.trans_t_dict[word_idx[len(word_idx)-1]][2]))
+				"""
 '''
 def write_output(stm_t_dict, output_file_name):
 	f = open(output_file_name, 'w')
@@ -300,15 +302,12 @@ if __name__ == "__main__":
 	recog_file_path = sys.argv[1]
 	trans_file_path = sys.argv[2]
 	label = sys.argv[3]
-	# start_offset = float(sys.argv[4])
-	# end_offset = float(sys.argv[5])
-	output_ctm_path = 'output_'+sys.argv[3]+'/'+sys.argv[3]+'.ctm'
-	output_stm_path = 'output_'+sys.argv[3]+'/'+sys.argv[3]+'_align.stm'
-	noise_itv = [[0, 185], [1494.5]]
+	output_stm_path = 'output_'+sys.argv[3]+'/'+sys.argv[3]+'.stm'
+	#noise_itv = [[0, 185], [1494.5]]
 	noise_file_path = sys.argv[6]
 
-	noise_inv = get_noise_itv(noise_file_path, 0.25)
-	# print(inv)
-	align = Alignment(label, recog_file_path, trans_file_path, output_stm_path, output_ctm_path, noise_itv)
+	noise_itv = get_noise_itv(noise_file_path, 0.25)
+	print(noise_itv)
+	align = Alignment(label, recog_file_path, trans_file_path, output_stm_path, noise_itv)
 	main(align)
 
